187f6e085fbf62fe36d1dcdf10cd7d98b407e777
HillBamboo <bamboohill@outlook.com>
2018-04-10 15:28:52 +0800

    重构豆瓣爬虫

--- 020douban/douban.py
+++ 020douban/douban.py
@@ -10,100 +10,117 @@ Info
 - author : "shjunlee"
 - email  : "shjunlee@foxmail.com"
 - date   : "2017.8.30"
+
+- HILLBAMBOO 重构于2018.4.9
 '''
 
 from urllib.request import urlretrieve
-import requests
-from bs4 import BeautifulSoup
 from os import remove
 try:
     import cookielib
 except:
     import http.cookiejar as cookielib
+
+import requests
+from bs4 import BeautifulSoup
 try:
     from PIL import Image
 except:
     pass
 
-url = 'https://accounts.douban.com/login'
 
-datas = {'source': 'index_nav',
-         'remember': 'on'}
+class DoubanLogin(object):
 
-headers = {'Host':'www.douban.com',
-           'Referer': 'https://www.douban.com/',
-           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-           'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
-           'Accept-Encoding':'gzip, deflate, br'}
+    def __init__(self):
+        self.session = requests.session()
+        self.session.cookies = cookielib.LWPCookieJar(filename='douban_cookies')
+        self.headers = {
+            'Host': 'www.douban.com',
+            'Referer': 'https://www.douban.com/',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:55.0) Gecko/20100101 Firefox/55.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3',
+            'Accept-Encoding': 'gzip, deflate, br'
+        }
+        self.login_url = "https://www.douban.com/login"
+        self.post_url = "https://accounts.douban.com/login"
+        self.auth_url = "https://www.douban.com/accounts/"
+        self.datas = {
+            'source': 'index_nav',
+            'remember': 'on'
+        }
+        # self.usr = usr
+        # self.pwd = pwd
 
-# 尝试使用cookie信息
-session = requests.session()
-session.cookies = cookielib.LWPCookieJar(filename='cookies')
-try:
-    session.cookies.load(ignore_discard=True)
-except:
-    print("Cookies未能加载")
-    #cookies加载不成功，则输入账号密码信息
-    datas['form_email'] = input('Please input your account:')
-    datas['form_password'] = input('Please input your password:')
+    def save_cookie(self):
+        self.session.cookies.save()
 
+    def load_cookie(self):
+        try:
+            self.session.cookies.load()
+        except:
+            print('cookie未能加载成功')
 
-def get_captcha():
-    '''
-    获取验证码及其ID
-    '''
-    r = requests.post(url, data=datas, headers=headers)
-    page = r.text
-    soup = BeautifulSoup(page, "html.parser")
-    # 利用bs4获得验证码图片地址
-    img_src = soup.find('img', {'id': 'captcha_image'}).get('src')
-    urlretrieve(img_src, 'captcha.jpg')
-    try:
-        im = Image.open('captcha.jpg')
-        im.show()
-        im.close()
-    except:
-        print('到本地目录打开captcha.jpg获取验证码')
-    finally:
-        captcha = input('please input the captcha:')
-        remove('captcha.jpg')
-    captcha_id = soup.find(
-        'input', {'type': 'hidden', 'name': 'captcha-id'}).get('value')
-    return captcha, captcha_id
+    def login(self):
+        if self.is_login():
+            print('登陆成功')
+        else:
+            usr = input('请输入用户名：\n>>> ')
+            pwd = input('请输入密码：\n>>> ')
+            self.datas['form_email'] = usr
+            self.datas['form_password'] = pwd
+            captcha, captcha_id = self.get_captcha()
+            # 增加表数据
+            self.datas['captcha-solution'] = captcha
+            self.datas['captcha-id'] = captcha_id
+            login_page = self.session.post(self.login_url, data=self.datas, headers=self.headers)
+            page = login_page.text
+            soup = BeautifulSoup(page, "html.parser")
+            result = soup.findAll('div', attrs={'class': 'title'})
 
+            # 进入豆瓣登陆后页面，打印热门内容
+            for item in result:
+                print(item.find('a').get_text())
 
-def isLogin():
-    '''
-    通过查看用户个人账户信息来判断是否已经登录
-    '''
-    url = "https://www.douban.com/accounts/"
-    login_code = session.get(url, headers=headers,
-                             allow_redirects=False).status_code
-    if login_code == 200:
-        return True
-    else:
-        return False
+            # 保存 cookies 到文件，下次可以使用 cookie 直接登录，不需要输入账号和密码
+            self.save_cookie()
 
+    def is_login(self):
+        '''
+        通过查看用户个人账户信息来判断是否已经登录
+        '''
+        self.load_cookie()
+        login_code = self.session.get(self.auth_url, headers=self.headers,
+                                 allow_redirects=False).status_code
+        if login_code == 200:
+            return True
+        else:
+            return False
 
-def login():
-    captcha, captcha_id = get_captcha()
-    # 增加表数据
-    datas['captcha-solution'] = captcha
-    datas['captcha-id'] = captcha_id
-    login_page = session.post(url, data=datas, headers=headers)
-    page = login_page.text
-    soup = BeautifulSoup(page, "html.parser")
-    result = soup.findAll('div', attrs={'class': 'title'})
-    #进入豆瓣登陆后页面，打印热门内容
-    for item in result:
-        print(item.find('a').get_text())
-    # 保存 cookies 到文件，
-    # 下次可以使用 cookie 直接登录，不需要输入账号和密码
-    session.cookies.save()
+    # TODO: 并不是每一次登录都需要输入验证码的，多次登录失败才需要输
+    def get_captcha(self):
+        '''
+        获取验证码及其ID
+        '''
+        r = requests.post(self.post_url, data=self.datas, headers=self.headers)
+        page = r.text
+        soup = BeautifulSoup(page, "html.parser")
+        # 利用bs4获得验证码图片地址
+        img_src = soup.find('img', {'id': 'captcha_image'}).get('src')
+        urlretrieve(img_src, 'captcha.jpg')
+        try:
+            im = Image.open('captcha.jpg')
+            im.show()
+            im.close()
+        except:
+            print('到本地目录打开captcha.jpg获取验证码')
+        finally:
+            captcha = input('please input the captcha:')
+            remove('captcha.jpg')
+        captcha_id = soup.find(
+            'input', {'type': 'hidden', 'name': 'captcha-id'}).get('value')
+        return captcha, captcha_id
 
 if __name__ == '__main__':
-    if isLogin():
-        print('Login successfully')
-    else:
-        login()
+    db = DoubanLogin()
+    db.login()
